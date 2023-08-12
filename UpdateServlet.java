57874d1c99b40bbb1cfea80314940635107160ae
Antikakumari <91655026+Antikakumari@users.noreply.github.com>
2023-08-12 11:40:07 +0530

    Add files via upload

---
+++ UpdateServlet.java
@@ -0,0 +1,59 @@
+package com.servlet;
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.entity.Note;
+import com.helper.FactoryProvider;
+
+/**
+ * Servlet implementation class UpdateServlet
+ */
+public class UpdateServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public UpdateServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		try {
+			String title = request.getParameter("title");
+			String content = request.getParameter("content");
+			int noteId = Integer.parseInt(request.getParameter("noteId").trim());
+			
+			Session se = FactoryProvider.getFactory().openSession();
+			Transaction tx = se.beginTransaction();
+			
+			Note note = se.get(Note.class,noteId);
+			note.setTitle(title);
+			note.setContent(content);
+			note.setAddedDate(new Date());
+			tx.commit();
+
+			se.close();
+			response.sendRedirect("show_note.jsp");
+			
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+}
---
+++ index.jsp
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+  <head>
+  
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    
+
+    <title>Welcome to our Notes!</title>
+    <link href="css/style.css" rel="stylesheet">
+    <%@include file="allcss.jsp" %>
+  </head>
+  <body>
+  <div class="container">
+  <%@include file="navbar.jsp" %>
+  
+    <h1>This is home page</h1><br>
+    <div class="card py-5">
+		<img alt="" class="img-fluid mx-auto" style="max-width:400px;" src="img/logo.jpeg">
+		<h1 class="text-primary text-uppercase text-center m-3">Start Taking Your Notes</h1>
+		<div class="container text-center">
+		<a href="add_note.jsp">
+		<button class="btn btn-outline-primary text-center">Start Here</button>
+		</a>
+		</div></div>
+   
+
+    <!-- Optional JavaScript -->
+    
+  </body>
+</html>
